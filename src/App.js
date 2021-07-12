ef9462dba562850857070c59d1c8446c1e6a2705
Hoerrin <patrykd0604@gmail.com>
2021-07-12 22:35:37 +0200

    Add comments

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import EmployeesList from './EmployeesList'
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import PerfectScrollbar from 'react-perfect-scrollbar'
 
+//This data would be fetched from API
+const employees = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania', 'Rafal', 'Marcin', 'Krzysztof', 'Grzegorz', 'Magdalena']
+
+
 
 function App() {
+  //Text according to which list will be filtered
   const [filter, setFilter] = useState('')
 
-  const employees = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania', 'Rafal', 'Marcin', 'Krzysztof', 'Grzegorz', 'Magdalena']
+  //TODO prepare for race conditions
   const employeesFiltered = filter ? employees.filter(name => name.toLowerCase().includes(filter.toLowerCase())) : employees
 
+  //Sets state to input value
   const handleInput = (event) => {
     setFilter(event.target.value)
   }
-
 
   return (
     <div className="app">
@@ -23,7 +28,7 @@ function App() {
           <input type="text" className="header__input" placeholder='Search...' value={filter} onChange={handleInput} />
         </div>
         <ul className="container__list">
-        <PerfectScrollbar options={{swipeEasing: true}}>
+        <PerfectScrollbar>
             <EmployeesList employeesFiltered={employeesFiltered} />
         </PerfectScrollbar>
         </ul>
